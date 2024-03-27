affced55d2cffe79a3126a60621a314200540538
Ian Silva <ssianfilipe@gmail.com>
2024-03-27 11:23:16 -0300

    create environmnet variables and validate with zod

--- src/database.ts
+++ src/database.ts
@@ -1,9 +1,14 @@
 import { knex as setupKnex, Knex } from 'knex'
+import { env } from './env'
+
+if (process.env.DATABASE_URL === undefined) {
+  throw new Error('DATABASE_URL env not found.')
+}
 
 export const config: Knex.Config = {
   client: 'sqlite',
   connection: {
-    filename: './db/app.db',
+    filename: env.DATABASE_URL,
   },
   migrations: {
     extension: 'ts',
--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,18 @@
 import fastify from 'fastify'
 import { knex } from './database'
+import { env } from './env'
 
 const app = fastify()
 
 app.get('/hello', async () => {
-  const tables = await knex('sqlite_schema').select('*')
+  const tables = await knex('transactions').select('*')
 
   return tables
 })
 
 app
   .listen({
-    port: 3333,
+    port: env.PORT,
   })
   .then(() => {
     console.log('HTTP Server Running!')
